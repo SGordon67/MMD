cc81aa2531b9b943921ba9e7f9da91eeb088d2e9
SGordon67 <SGordon031@gmail.com>
2025-06-17 21:26:19 -0400

    Large restructure. JSON fully adopted for data transfer for both directions. Data about clients being held in a more simple manner rather than having a full buffer for each client.

--- client.cpp
+++ client.cpp
@@ -11,75 +11,92 @@
 using json = nlohmann::json;
 
 int main(){
-	initscr();
-	cbreak();
-	noecho();
-	scrollok(stdscr, TRUE);
-	nodelay(stdscr, TRUE);
-	refresh();
+    initscr();
+    cbreak();
+    noecho();
+    scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    refresh();
 
-	// creating socket
-	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // creating socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	// specifying address
-	sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(8080);
-	inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr);
+    // specifying address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr);
 
-	// sending connection request
-	connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    // sending connection request
+    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
-	// poll setup
-	struct pollfd fds[1];
-	fds[0].fd = clientSocket;
-	fds[0].events = POLLIN;
+    // poll setup
+    struct pollfd fds[1];
+    fds[0].fd = clientSocket;
+    fds[0].events = POLLIN;
 
-	char buffer[1024] = { 0 };
-	while(true){
-		// see if server needs to update
-		int ret = poll(fds, 1, 100);
-		if(ret > 0){
-			// Get input from server
-			int bytes = read(clientSocket, buffer, sizeof(buffer));
-			buffer[bytes] = '\0';
-			if(bytes <= 0){
-				nodelay(stdscr, FALSE);
-				mvprintw(12, 0, "Server disconnected. Press any key to exit.");
-				refresh();
-				getch();
-				break;
-			}
-			mvprintw(5, 0, "Update from server!");
-			mvprintw(6, 0, "Most Rencent press:");
-			mvprintw(6, 21, "%s", buffer);
-			refresh();
-		}
-		// getting input
-		std::string message;
-		int input = getch();
-		if(input != ERR){
-			mvprintw(0, 0, "You Pressed:");
-			mvprintw(0, 13, "%c", char(input));
-			mvprintw(10, 0, "Input--Detected");
-			refresh();
-			// construct json to send to server based on input
-			json j;
-			j["type"] = "keypress";
-			j["value"] = std::string(1, input);
-			std::string message = j.dump();
-			send(clientSocket, message.c_str(), message.size(), 0);
-			// message = std::string(1, input);
-			// send(clientSocket, message.c_str(), message.size(), 0);
-		}else{
-			mvprintw(10, 0, "----Waiting----");
-			refresh();
-		}
-	}
+    char buffer[1024] = { 0 };
+    while(true){
+        // see if server needs to update
+        int ret = poll(fds, 1, 100);
+        if(ret > 0){
+            // Get input from server
+            int bytes = read(clientSocket, buffer, sizeof(buffer));
+            if(bytes <= 0){
+                nodelay(stdscr, FALSE);
+                mvprintw(12, 0, "Server disconnected. Press any key to exit.");
+                refresh();
+                getch();
+                break;
+            }
+            if(bytes > 0 && bytes < sizeof(buffer)){
+                buffer[bytes] = '\0';
+                try{
+                    // mvprintw(15, 0, "Raw buffer: %s", buffer);
+                    json j = json::parse(buffer);
+                    int row = 12;
+                    mvprintw(row++, 0, "Server Update:\n");
+                    for(const auto& client : j["clients"]){
+                        int id = client["id"].get<int>();
+                        char input = client["last_input"].get<std::string>()[0];
+                        mvprintw(row++, 0, "Client %d pressed: %c", id, input);
+                        refresh();
+                    }
+                } catch(json::parse_error &e){
+                    mvprintw(13, 0, "[DEBUG] In catch statement for parsing input");
+                    refresh();
+                }
+            }
+            // mvprintw(5, 0, "Update from server!");
+            // mvprintw(6, 0, "Most Rencent press:");
+            // mvprintw(6, 21, "%s", buffer);
+            // refresh();
+        }
+        // getting input
+        std::string message;
+        int input = getch();
+        if(input != ERR){
+            mvprintw(0, 0, "You Pressed:");
+            mvprintw(0, 13, "%c", char(input));
+            mvprintw(10, 0, "Input--Detected");
+            refresh();
+            // construct json to send to server based on input
+            json j;
+            j["type"] = "keypress";
+            j["input"] = std::string(1, input);
+            std::string message = j.dump();
+            send(clientSocket, message.c_str(), message.size(), 0);
+            // message = std::string(1, input);
+            // send(clientSocket, message.c_str(), message.size(), 0);
+        }else{
+            mvprintw(10, 0, "----Waiting----");
+            refresh();
+        }
+    }
 
-	// closing socket
-	endwin();
-	close(clientSocket);
+    // closing socket
+    endwin();
+    close(clientSocket);
 }
 
 
--- server.cpp
+++ server.cpp
@@ -1,4 +1,4 @@
-// TODO
+// TODO:
 // 1. make server -> client communication into JSON
 //		^ currently broken even without json
 // 2. add keypress info to JSON structure
@@ -18,127 +18,141 @@
 using json = nlohmann::json;
 
 struct ClientInfo{
-	int fd;
-	std::array<char, 1024> buffer;
+    int fd;
+    char last_input;
 };
 
 int main(){
-	// delay for game updates
-	auto updateDelay = std::chrono::seconds(3);
-	auto last_sent = std::chrono::steady_clock::now();
+    // delay for game updates
+    auto updateDelay = std::chrono::seconds(3);
+    auto last_sent = std::chrono::steady_clock::now();
 
-	// Make sure to keep the server open until at lease one client has connected.
-	bool anyClientEverConnected = false;
+    // Make sure to keep the server open until at lease one client has connected.
+    bool anyClientEverConnected = false;
 
-	// create a vector of buffers for each of the clients
-	std::vector<ClientInfo> clients;
+    // create a vector of buffers for each of the clients
+    std::vector<ClientInfo> clients;
 
-	// create server socket
-	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(serverSocket < 0){
-		perror("socket creation failed");
-		return 1;
-	}
+    // create server socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverSocket < 0){
+        perror("socket creation failed");
+        return 1;
+    }
 
-	int opt = 1;
-	setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    int opt = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-	// specifying address
-	sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(8080);
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
+    // specifying address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-	// binding the socket
-	if(bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) > 0){
-		perror("Bind failed");
-		close(serverSocket);
-		return 1;
-	}
+    // binding the socket
+    if(bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) > 0){
+        perror("Bind failed");
+        close(serverSocket);
+        return 1;
+    }
 
-	// listening to the assigned socket
-	if(listen(serverSocket, 5) < 0){
-		perror("Listen failed");
-		close(serverSocket);
-		return 1;
-	}
+    // listening to the assigned socket
+    if(listen(serverSocket, 5) < 0){
+        perror("Listen failed");
+        close(serverSocket);
+        return 1;
+    }
 
-	while(true){
-		std::vector<pollfd> pollFds;
-		pollFds.push_back({serverSocket, POLLIN, 0});
-		for(const auto& client : clients){
-			pollFds.push_back({client.fd, POLLIN, 0});
-		}
-		int ret = poll(pollFds.data(), pollFds.size(), 100);
-		if(ret > 0){
-			// check for a new connection
-			if(pollFds[0].revents & POLLIN){
-				int newClient = accept(serverSocket, nullptr, nullptr);
-				if(newClient >= 0){
-					pollFds.push_back({newClient, POLLIN, 0});
-					clients.push_back({newClient, {}});
-					anyClientEverConnected = true;
-				}
-			}
-			for(int i = pollFds.size() - 1; i >= 1; i--){
-				// detect hangup or disconnect error
-				if(pollFds[i].revents & (POLLHUP | POLLERR)){
-					std::cout << "[INFO] Client disconnected (poll flags): " << pollFds[i].fd << "\n";
-					close(pollFds[i].fd);
-					pollFds.erase(pollFds.begin() + i);
-					clients.erase(clients.begin() + (i-1));
-					continue;
-				}
-				// check for readable input
-				if(pollFds[i].revents & POLLIN){
-					// Get input from client
-					int clientIndex = i-1;
-					ClientInfo client = clients[clientIndex];
-					int bytes = read(client.fd, client.buffer.data(), client.buffer.size() - 1);
-					if(bytes <= 0){
-						std::cout << "[INFO] Client disconnected (read failure): " << client.fd << "\n";
-						close(client.fd);
-						pollFds.erase(pollFds.begin() + i);
-						clients.erase(clients.begin() + clientIndex);
-						continue;
-					}
-					if(bytes > 0 && bytes < client.buffer.size()){
-						// std::cout << "[DEBUG] Received raw: '" << client.buffer.data() << "'\n";
-						try{
-							std::string jsonStr(client.buffer.data(), bytes);
-							auto j = json::parse(jsonStr);
-							auto it = j.find("value");
-							if(it != j.end() && it->is_string()){
-								std::string val = *it;
-								std::cout << "[INFO] Client " << i << " Pressed " << val << "\n";
-							} else{
-								std::cout << "[WARNING] Unkown message format.\n";
-							}
-						} catch(json::parse_error &e){
-							std::cout << "[DEBUG] In catch statement for parsing input\n";
-							std::cerr << "JSON parse error: " << e.what() << "\n";
-						}
-					}
-					// std::cout << "[INFO] Client " << i << " Pressed " << buffers[i].data() << "\n";
-				}
-			}
-		}
-		// close the server if no clients remain
-		if(anyClientEverConnected && clients.empty() == 1){
-			std::cout << "[INFO] All clients disconnected. Shutting down server.\n";
-			break;
-		}
-		auto now = std::chrono::steady_clock::now();
-		if(now - last_sent >= updateDelay){
-			for(auto& client : clients){
-				send(client.fd, client.buffer.data(), client.buffer.size(), 0);
-				// send(mySockets[i].fd, buffers[i].data(), sizeof(buffers[i]), 0);
-			}
-			last_sent = now;
-		}
-	}
-	close(serverSocket);
-	return 0;
+    while(true){
+        std::vector<pollfd> pollFds;
+        pollFds.push_back({serverSocket, POLLIN, 0});
+        for(const auto& client : clients){
+            pollFds.push_back({client.fd, POLLIN, 0});
+        }
+        int ret = poll(pollFds.data(), pollFds.size(), 100);
+        if(ret > 0){
+            // check for a new connection
+            if(pollFds[0].revents & POLLIN){
+                int newClient = accept(serverSocket, nullptr, nullptr);
+                if(newClient >= 0){
+                    pollFds.push_back({newClient, POLLIN, 0});
+                    clients.push_back({newClient, '\0'});
+                    anyClientEverConnected = true;
+                }
+            }
+            for(int i = pollFds.size() - 1; i >= 1; i--){
+                // detect hangup or disconnect error
+                if(pollFds[i].revents & (POLLHUP | POLLERR)){
+                    std::cout << "[INFO] Client disconnected (poll flags): " << pollFds[i].fd << "\n";
+                    close(pollFds[i].fd);
+                    pollFds.erase(pollFds.begin() + i);
+                    clients.erase(clients.begin() + (i-1));
+                    continue;
+                }
+                // check for readable input
+                if(pollFds[i].revents & POLLIN){
+                    // Get input from client
+                    int clientIndex = i-1;
+                    ClientInfo client = clients[clientIndex];
+                    char tempBuffer[1024] = {0};
+                    int bytes = read(client.fd, tempBuffer, sizeof(tempBuffer) - 1);
+                    if(bytes <= 0){
+                        std::cout << "[INFO] Client disconnected (read failure): " << client.fd << "\n";
+                        close(client.fd);
+                        pollFds.erase(pollFds.begin() + i);
+                        clients.erase(clients.begin() + clientIndex);
+                        continue;
+                    }
+                    if(bytes > 0){
+                        tempBuffer[bytes] = '\0';
+                        // std::cout << "[DEBUG] Received raw: '" << client.buffer.data() << "'\n";
+                        try{
+                            std::string jsonStr(tempBuffer, bytes);
+                            auto j = json::parse(jsonStr);
+                            auto it = j.find("input");
+                            if(it != j.end() && it->is_string()){
+                                std::string val = *it;
+                                clients[clientIndex].last_input = val[0];
+                                std::cout << "[INFO] Client " << client.fd << " Pressed " << val[0] << "\n";
+                            } else{
+                                std::cout << "[WARNING] Unkown message format.\n";
+                            }
+                        } catch(json::parse_error &e){
+                            std::cout << "[DEBUG] In catch statement for parsing input\n";
+                            std::cerr << "[ERROR] JSON parse error: " << e.what() << "\n";
+                        }
+                    }
+                    // std::cout << "[INFO] Client " << i << " Pressed " << buffers[i].data() << "\n";
+                }
+            }
+        }
+        // close the server if no clients remain
+        if(anyClientEverConnected && clients.empty() == 1){
+            std::cout << "[INFO] All clients disconnected. Shutting down server.\n";
+            break;
+        }
+        auto now = std::chrono::steady_clock::now();
+        if(now - last_sent >= updateDelay){
+            // construct json to send to server based on input
+            json update_j;
+            update_j["type"] = "update";
+            update_j["clients"] = json::array();
+            for(const auto& client : clients){
+                json client_info;
+                client_info["id"] = client.fd;
+                client_info["last_input"] = std::string(1, client.last_input);
+                update_j["clients"].push_back(client_info);
+                // std::cout << "PUSHING INFO:\nCLIENT: " + std::to_string(client.fd) + "\nLAST_INPUT: " + client.last_input + "\n";
+            }
+            std::string sj = update_j.dump();
+            for(auto& client : clients){
+                send(client.fd, sj.c_str(), sj.size(), 0);
+            }
+            last_sent = now;
+        }
+    }
+    close(serverSocket);
+    return 0;
 }
 
 
